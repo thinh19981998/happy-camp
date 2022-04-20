010a79657e521112b0147b1d6fdc80db50055562
Doan Phu Thinh <dpthinh1508@gmail.com>
2022-04-20 16:27:31 +0700

    fix error

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp_camp' || process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp_camp';
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
